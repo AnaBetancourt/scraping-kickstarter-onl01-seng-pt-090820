e75ed0ed722e1fed17edd4faa9fe660918391855
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-11 05:54:53 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,4 +9,4 @@ end
 
 create_project_hash
 # projects: kickstarter.css("li.project.grid_4")grid_4
-# project.css("h2.bbcard_name strong a").text
+# title = project.css("h2.bbcard_name strong a").text
